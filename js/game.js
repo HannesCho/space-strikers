e8a71857215297f092ea1f076a5e7ef70056510a
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-08-23 11:48:39 +0200

    add background music

--- js/game.js
+++ js/game.js
@@ -3,11 +3,13 @@ class Game {
     constructor () {
         this.backgroundImages = [];
         this.playerImage = null;
+        this.backgroundMusic = null;
     }
 
     setup () {
         this.background = new Background;
         this.player = new Player;
+        this.backgroundMusic = new Audio('../assets/sounds/POL-waving-grass-short.wav')
     }
 
     preload () {
@@ -20,5 +22,10 @@ class Game {
     draw() {
         this.background.draw()
         this.player.draw()
+        //background music setting
+        this.backgroundMusic.loop = true;
+        this.backgroundMusic.volume = 0.05;
+        this.backgroundMusic.play();
     }
+
 }
