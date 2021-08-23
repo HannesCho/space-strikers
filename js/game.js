5acacccd481f9f9c8f5aac0317b1bc836b5e58dd
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-08-23 11:22:04 +0200

    setup js

---
+++ js/game.js
@@ -0,0 +1,11 @@
+class Game {
+
+    constructor () {
+        this.backgroundImages = [];
+        this.playerImage = null;
+    }
+
+    setup () {
+        this.background = new Background;
+        this.player = new Player;
+    }
---
+++ js/main.js
@@ -0,0 +1,15 @@
+const game = new Game;
+
+function preload() {
+    game.preload()
+}
+
+function setup() {
+    createCanvas(600, 600)
+    game.setup()
+}
+
+function draw() {
+    game.draw();
+}
+
